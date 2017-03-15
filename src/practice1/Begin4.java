d9433793c511476df1dac851b40b6c3663a8be18
Arthur54481 <arthur54481@gmail.com>
2017-03-15 22:30:49 +0800

    入门训练

---
+++ src/practice1/Begin4.java
@@ -0,0 +1,19 @@
+package practice1;
+import java.util.Scanner; 
+public class Begin4{ 
+  public static void main(String[] args) { 
+    Scanner input = new Scanner(System.in); 
+    int n = input.nextInt(); 
+    int a=1,b=1,num = 0; 
+    if(n==1 || n==2) 
+      System.out.printf("1\n"); 
+      else{ 
+        for(int i=3;i<=n;i++){ 
+          num=(a+b)%10007; 
+          a=b; 
+          b=num; 
+      } 
+     System.out.println(num); 
+    } 
+  } 
+}
