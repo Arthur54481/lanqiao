22297c9e0b128c0e7dfe34f57bb2da4e81188292
Arthur54481 <arthur54481@gmail.com>
2017-04-18 21:47:39 +0800

    回文数

--- src/practice2/Basic8.java
+++ src/practice2/Basic8.java
@@ -1,5 +1,10 @@
 package practice2;
-
+/*
+问题描述
+　　1221是一个非常特殊的数，它从左边读和从右边读是一样的，编程求所有这样的四位十进制数。
+输出格式
+　　按从小到大的顺序输出满足条件的四位十进制数。
+ */
 public class Basic8 {
     public static void main(String[] args){
     	for(int a=1000;a<10000;a++){
